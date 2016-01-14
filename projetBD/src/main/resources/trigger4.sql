9ee8e1c415f06cc476d2010161ac64190da6c090
thiam <thiam@130.190.31.140>
2016-01-14 14:09:19 +0100

    Trigger 4 et 5

--- projetBD/src/main/resources/trigger4.sql
+++ projetBD/src/main/resources/trigger4.sql
@@ -1,9 +1,12 @@
-Create or Replace TRIGGER trigger4
+--Pour une commande dépassant les 100 euros, un code promo est généré.
+--Sa valeur est de 5% de la commande ayant donné lieu à ce code. (SGBD trigger) 
+
+Create or Replace TRIGGER G4
 After insert or Update on Orders
 For each row
 BEGIN
 	if (Orders.TotalPrice > 100)
-		 update CodePromo set amout= TotalPrice * 0.05 where CodePromo.IdClient = Client. IdClient 
+		 update CodePromo set amount = TotalPrice * 0.05 where CodePromo.IdClient = Client.IdClient 
 		 and Client.IdClient = Orders.IdCleint;
 	end if;	
 END;

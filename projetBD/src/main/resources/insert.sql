893dba30a25e8bad347289f04b79d1be493a9c08
smaleks <smaleks@130.190.30.108>
2016-01-19 14:15:53 +0100

    gt

--- projetBD/src/main/resources/insert.sql
+++ projetBD/src/main/resources/insert.sql
@@ -5,7 +5,7 @@ insert into Client (IdClient, FirstName, LastName, Mail, Password, Address) valu
 
 insert into Formats (IdFormat, Label, Price, ResolutionMin, Speed, Stock) values(IdFormat.NEXTVAL,'A3',2,10,1000,20);
 insert into Formats (IdFormat, Label, Price, ResolutionMin, Speed, Stock) values(IdFormat.NEXTVAL,'A4',0.2,6,1700,1000);
-insert into Formats values(IdFormat.NEXTVAL,'A5',2,4,'20000','1000');
+insert into Formats values(IdFormat.NEXTVAL,'A5',2,4,20000,1000);
 
 insert into Prestataire (IdPrestataire, NamePresta, AddressPresta, Preference) values(IdPrestataire.NEXTVAL,'ImprimeFAU','Annecy',1);
 insert into Prestataire (IdPrestataire, NamePresta, AddressPresta, Preference) values(IdPrestataire.NEXTVAL,'PrinterSA','Grenoble',2);
@@ -14,5 +14,8 @@ insert into Prestataire (IdPrestataire, NamePresta, AddressPresta, Preference) v
 insert into Image (IdImage, IdClient, PathImage, Shared, ResolutionImage, Info) values(IdImage.NEXTVAL,1,'./image', 0,5,'Vancances plage ');
 insert into Image (IdImage, IdClient, PathImage, Shared, ResolutionImage, Info) values(IdImage.NEXTVAL,2,'./image',1,20,'Vancances Berlin ');
 insert into Image (IdImage, IdClient, PathImage, Shared, ResolutionImage, Info) values(IdImage.NEXTVAL,2,'./images',0,16,'Vancances hiver ');
+
+
+
 
 
